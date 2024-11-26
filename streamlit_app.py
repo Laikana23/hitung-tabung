f3d6069128abde387ab6f88686ae835f311abdaf
Laikana23 <154624899+Laikana23@users.noreply.github.com>
2024-11-26 16:52:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,5 @@ r = st.number_input("Masukan Jari-Jari: ",0)
 m = st.number_input("Masukan Panjang: ",0)
 
 if st.button("Hitung-Volume", type="primary"):
-  V = math.pi*(r**5)*m
+  v = math.pi*(r**5)*m
   st.success(f'volume tabung adalah {v:.2f}')

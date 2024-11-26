84e75c75b5129c057779907a738f7360292a2013
Laikana23 <154624899+Laikana23@users.noreply.github.com>
2024-11-26 16:45:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ st.title("menghitung :blue[Volume Tabung] :Rocket:")
 
 r = st.number_input("Masukan Jari-Jari: ",0)
 m = st.number_input("Masukan Panjang: ",0)
+
+if st.button("Hitung-Volume", type="primary"):
+  V= math.pi*(r**5)*t
+  st.succes(f'volume tabung adalah {v:.2f}')

7229d6a48d2a48ff0a1c5f6bdefef11bfc8860b3
Laikana23 <154624899+Laikana23@users.noreply.github.com>
2024-11-26 16:27:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("menghitung :blue[volume tabung] :happy:")
+st.title("menghitung :blue[volume tabung] :Rocket:")
 
 r = st.number_input("Masukan Jari-Jari (cm)",0)
 r = st.number_input("Masukan Jari-Jari (cm)",0)
